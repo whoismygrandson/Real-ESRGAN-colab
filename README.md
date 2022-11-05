ff251617eafc640418972469a285c772f6423471
whoismygrandson <61721162+whoismygrandson@users.noreply.github.com>
2022-11-05 23:18:08 +0800

    add

---
+++ README.md
@@ -0,0 +1,36 @@
+---
+title: Real ESRGAN
+emoji: 🏃
+colorFrom: blue
+colorTo: blue
+sdk: gradio
+sdk_version: 3.1.7
+app_file: app.py
+pinned: false
+---
+
+### 这个是fork的`https://huggingface.co/spaces/akhaliq/Real-ESRGAN`，目的是搭建个人API来使用，因为我不是很确定这个平台是否收费，会不会对原作者产生经济上的损失。补充：原仓库采用BSD-3-Clause开源协议
+
+# Configuration
+
+`title`: _string_  
+Display title for the Space
+
+`emoji`: _string_  
+Space emoji (emoji-only character allowed)
+
+`colorFrom`: _string_  
+Color for Thumbnail gradient (red, yellow, green, blue, indigo, purple, pink, gray)
+
+`colorTo`: _string_  
+Color for Thumbnail gradient (red, yellow, green, blue, indigo, purple, pink, gray)
+
+`sdk`: _string_  
+Can be either `gradio` or `streamlit`
+
+`app_file`: _string_  
+Path to your main application file (which contains either `gradio` or `streamlit` Python code).  
+Path is relative to the root of the repository.
+
+`pinned`: _boolean_  
+Whether the Space stays on top of your list.
---
+++ app.py
@@ -0,0 +1,68 @@
+import os
+os.system("pip install gradio==2.9b23")
+import random
+import gradio as gr
+from PIL import Image
+import torch
+from random import randint
+import sys
+from subprocess import call
+import psutil
+
+
+
+
+torch.hub.download_url_to_file('http://people.csail.mit.edu/billf/project%20pages/sresCode/Markov%20Random%20Fields%20for%20Super-Resolution_files/100075_lowres.jpg', 'bear.jpg')
+  
+    
+def run_cmd(command):
+    try:
+        print(command)
+        call(command, shell=True)
+    except KeyboardInterrupt:
+        print("Process interrupted")
+        sys.exit(1)
+run_cmd("wget https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth -P .")
+run_cmd("pip install basicsr")
+run_cmd("pip freeze")
+
+os.system("wget https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.2.4/RealESRGAN_x4plus_anime_6B.pth -P .")
+
+
+def inference(img,mode):
+    _id = randint(1, 10000)
+    INPUT_DIR = "/tmp/input_image" + str(_id) + "/"
+    OUTPUT_DIR = "/tmp/output_image" + str(_id) + "/"
+    run_cmd("rm -rf " + INPUT_DIR)
+    run_cmd("rm -rf " + OUTPUT_DIR)
+    run_cmd("mkdir " + INPUT_DIR)
+    run_cmd("mkdir " + OUTPUT_DIR)
+    basewidth = 256
+    wpercent = (basewidth/float(img.size[0]))
+    hsize = int((float(img.size[1])*float(wpercent)))
+    img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+    img.save(INPUT_DIR + "1.jpg", "JPEG")
+    if mode == "base":
+        run_cmd("python inference_realesrgan.py -n RealESRGAN_x4plus -i "+ INPUT_DIR + " -o " + OUTPUT_DIR)
+    else:
+        os.system("python inference_realesrgan.py -n RealESRGAN_x4plus_anime_6B -i "+ INPUT_DIR + " -o " + OUTPUT_DIR)
+    return os.path.join(OUTPUT_DIR, "1_out.jpg")
+
+
+
+        
+title = "Real-ESRGAN"
+description = "Gradio demo for Real-ESRGAN. To use it, simply upload your image, or click one of the examples to load them. Read more at the links below. Please click submit only once"
+article = "<p style='text-align: center'><a href='https://arxiv.org/abs/2107.10833'>Real-ESRGAN: Training Real-World Blind Super-Resolution with Pure Synthetic Data</a> | <a href='https://github.com/xinntao/Real-ESRGAN'>Github Repo</a></p>"
+
+gr.Interface(
+    inference, 
+    [gr.inputs.Image(type="pil", label="Input"),gr.inputs.Radio(["base","anime"], type="value", default="base", label="model type")], 
+    gr.outputs.Image(type="file", label="Output"),
+    title=title,
+    description=description,
+    article=article,
+    examples=[
+    ['bear.jpg','base'],
+    ['anime.png','anime']
+    ]).launch(share=True)
